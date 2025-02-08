3da8c783e0ca28781f8272b24732586acc6c2408
Ketan9548 <kc918062@gmail.com>
2025-02-08 10:26:23 +0530

    Done Changes db

--- Server/DatabaseConnection/DbConnection.js
+++ Server/DatabaseConnection/DbConnection.js
@@ -6,7 +6,13 @@ const sequelizeDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proc
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306,
-    logging:false,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   });
 
 export default sequelizeDB;
