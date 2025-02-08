434e77f800cbafc0e0aaf894f7fb3fbdaad52f8a
Ketan9548 <kc918062@gmail.com>
2025-02-08 10:37:55 +0530

    DB changes

--- Server/DatabaseConnection/DbConnection.js
+++ Server/DatabaseConnection/DbConnection.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 const sequelizeDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DIALECT,
     port: process.env.DB_PORT || 3306,
     logging: false,
     dialectOptions: {
