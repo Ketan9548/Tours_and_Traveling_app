c18f91f989fd49beb998c7f2163ad132a3b521d8
Ketan9548 <kc918062@gmail.com>
2025-02-08 10:20:23 +0530

    Done Changes

--- Server/DatabaseConnection/DbConnection.js
+++ Server/DatabaseConnection/DbConnection.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config'
 
-const sequelizeDB = new Sequelize(process.env.dbname, process.env.DB_USER, process.env.dbpassword,
+const sequelizeDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306,
+    logging:false,
   });
 
 export default sequelizeDB;
