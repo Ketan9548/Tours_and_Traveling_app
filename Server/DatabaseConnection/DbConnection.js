cf6d787f988b148448e7299c69f24d8b395b6a07
Ketan9548 <kc918062@gmail.com>
2025-02-08 14:24:13 +0530

    Done changes backend

--- Server/DatabaseConnection/DbConnection.js
+++ Server/DatabaseConnection/DbConnection.js
@@ -1,18 +1,16 @@
 import { Sequelize } from "sequelize";
-import 'dotenv/config'
+import 'dotenv/config';
 
-const sequelizeDB = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
+const sequelizeDB = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DIALECT,
+    dialect: process.env.DIALECT || "mysql",
     port: process.env.DB_PORT || 3306,
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  });
+  }
+);
 
 export default sequelizeDB;
